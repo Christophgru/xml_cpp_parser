79627ec279f5377acc658393a17590117464eeb4
Jonas <Jonas.Heiss@hotmail.com>
2022-06-08 11:56:15 +0200

    Doxygen added

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/chris/OneDrive/Dokumente/DHBW/Programmieren/c_cpp/xmlparser_abgabe
+# Install script for directory: C:/Users/Jonas/CLionProjects/xml_cpp_parser
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/chris/OneDrive/Dokumente/DHBW/Programmieren/c_cpp/xmlparser_abgabe/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Jonas/CLionProjects/xml_cpp_parser/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
