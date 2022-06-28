4aad4e99345fa9cc18a68fa5460b659438f65a93
Christophgru <christoph@familie-gruender.de>
2022-06-28 20:29:27 +0200

    readXML.h angepasst

--- scr/include/XMLread/readXML.h
+++ scr/include/XMLread/readXML.h
@@ -8,7 +8,7 @@
 #include "../../XMLparser.h"
 
 
-void readXML(const std::string& path, DHBW::filedata& tofill);
+void readXML(std::string path, DHBW::filedata& tofill);
 
 
 #endif //XMLPARSER_ABGABE_RADXML_H
