1945de072e5ef4a367f3dd8677a3c7d20a28d3c4
Christophgru <christoph@familie-gruender.de>
2022-07-23 10:15:01 +0200

    stderr bei exception error added

--- output/Cfiles/abstractXMLparser.cpp
+++ output/Cfiles/abstractXMLparser.cpp
@@ -1,7 +1,7 @@
 //
-//This empty method body for the classabstractXMLparser.cpp was created by Jonas Heiss on 07/21/2022 contact me under015207619839
+//This empty method body for the classabstractXMLparser.cpp was created by Jonas Heiss on 07/22/2022 contact me under015207619839
 //
-#include "../Hfiles/abstractXMLparser.h" 
+#include "../Hfiles/abstractXMLparser.h"
 #include <getopt.h>
 
 #include <iostream>
@@ -10,56 +10,110 @@ using namespace std;
 
 
 void DHBW::abstractXMLparser::parseOptions(int argc, char **argv) {
-workingdir=argv[0];int i;
+    workingdir = argv[0];
+    int i;
     int optindex;
-bool h_flag = false;bool v_flag = false;bool g_flag = false;
+    bool h_flag = false;
+    bool v_flag = false;
+    bool g_flag = false;
+    bool s_flag = false;
     const struct option longopts[] =
-            {{"help", no_argument, nullptr, 'h'},{"generate", required_argument, nullptr, 'g'}
-};
+            {{"help",        no_argument,       nullptr, 'h'},
+             {"generate",    required_argument, nullptr, 'g'},
+             {"signperline", no_argument,       nullptr, 's'}
+            };
 
-while ((i = getopt_long(argc, argv, "hvg:", longopts, &optindex)) >=0)
-switch(i){
+    while ((i = getopt_long(argc, argv, "hvg:s", longopts, &optindex)) >= 0)
+        switch (i) {
 
-case 'h':
-h_flag =true;
- break;
-case 'v':
-v_flag =true;
- break;
-case 'g':
- path=argv[argc-1];
-g_flag =true;
- break;default:
-break;
+            case 'h':
+                h_flag = true;
+                break;
+            case 'v':
+                v_flag = true;
+                break;
+            case 'g':
+                path = argv[argc - 1];
+                g_flag = true;
+                break;
+            case 's':
+                signperline = atoi(argv[argc - 1]);
+                s_flag = true;
+                break;
+            default:
+                break;
+        }
+
+    if (h_flag) {
+        if (v_flag || g_flag) {
+            cout << "Exclusion Error: h" << endl;
+            cerr << "Exclusion Error " << endl;
+        } else { printhelp(); }
+    }
+    if (v_flag) {
+        if (h_flag || g_flag) {
+            cout << "Exclusion Error: v" << endl;
+            cerr << "Exclusion Error " << endl;
+        } else { printversion(); }
+    }
+    if (g_flag) {
+        if (h_flag || v_flag) {
+            cout << "Exclusion Error: g" << endl;
+            cerr << "Exclusion Error " << endl;
+        } else { generate(path); }
+    }
+    if (s_flag) {
+        if (g_flag) {
+            cout << "Exclusion Error: s" << endl;
+            cerr << "Exclusion Error " << endl;
+        } else { setSignPerLine(); }
+    }
 }
 
-if(h_flag){
-if(  v_flag ||g_flag ){
- cout << "Exclusion Error: h" << endl;
- cerr << "Exclusion Error "<<endl;
-}else{printhelp(); }
-}if(v_flag){
-if(  h_flag ||g_flag ){
- cout << "Exclusion Error: v" << endl;
- cerr << "Exclusion Error "<<endl;
-}else{printversion(); }
-}if(g_flag){
-if(  h_flag ||v_flag ){
- cout << "Exclusion Error: g" << endl;
- cerr << "Exclusion Error "<<endl;
-}else{generate(path); }
-}}void DHBW::abstractXMLparser::printhelp() {
-printf("%s",helptext.data());
-}bool DHBW::abstractXMLparser::isSethelptext() {
-    return !helptext.empty();
-}void DHBW::abstractXMLparser::printversion() {
-printf("Your current Version is: %s",version.data());
-}bool DHBW::abstractXMLparser::isSetversion() {
-    return !version.empty();
-}string DHBW::abstractXMLparser::getValueOfpath(){
-if(path!="-"||path!= ""){return path;} return path;
+string helptextformatierung(string text, int signsperline) {
+    int space_count = 0;
+    bool check = false;
+    int count = -1;
+    for (int i = 0; i < text.length(); ++i) {
+        count++;
+        if (text[i] != ' ' && check) {
+            space_count++;
+        } else {
+            space_count = 0;
+            check = true;
+        }
+        if (count % signsperline == 0 && i != 0) {
+            text[i - space_count] = '\n';
+            count = space_count;
+        }
+    }
+}
+
+void DHBW::abstractXMLparser::printhelp() {
+    printf("%s", helptextformatierung(helptext, signperline).data());
+}
+
+bool DHBW::abstractXMLparser::isSethelptext() {
+    return !(helptext.empty());
+}
+
+void DHBW::abstractXMLparser::printversion() {
+    printf("Your current Version is: %s", version.data());
+}
+
+bool DHBW::abstractXMLparser::isSetversion() {
+    return !(version.empty());
+}
+
+string DHBW::abstractXMLparser::getValueOfpath() {
+    if (path != "-" || path != "") { return path; }
+    return path;
 }
 
 bool DHBW::abstractXMLparser::isSetpath() {
-    return !path.empty();
+    return !(path.empty());
+}
+
+bool DHBW::abstractXMLparser::isSetsignperline() {
+    return !(signperline != 79);
 }
--- output/Hfiles/abstractXMLparser.h
+++ output/Hfiles/abstractXMLparser.h
@@ -1,5 +1,5 @@
 //
-//This empty method body for the classabstractXMLparser.h was created by Jonas Heiss on 07/21/2022
+//This empty method body for the classabstractXMLparser.h was created by Jonas Heiss on 07/22/2022
 //
 
 #ifndef abstractXMLparser_H
@@ -14,49 +14,58 @@ namespace DHBW {
 //Sobald ein Methodenaufruf abstrakt ist, wird die Basisklasse abstrakt.
 //Fuer die Formatierung der generierten Dateien wird astyle verwendet.
 
-class abstractXMLparser{
+    class abstractXMLparser {
 
- public:
- std::string workingdir;
-const std::string helptext="Overall Description:"
-"Erstellt einen Rumpf zum einlesen von Argumente aus der Kommandozeile."
-"Es kann sowohl mit innenliegenden Container wie externer Klassenanbindung eine Datenhaltung erfolgen."
-"Sobald ein Methodenaufruf abstrakt ist, wird die Basisklasse abstrakt."
-"Fuer die Formatierung der generierten Dateien wird astyle verwendet."
+    public:
+        std::string workingdir;
+        const std::string helptext = "Overall Description:"
+                                     "Erstellt einen Rumpf zum einlesen von Argumente aus der Kommandozeile."
+                                     "Es kann sowohl mit innenliegenden Container wie externer Klassenanbindung eine Datenhaltung erfolgen."
+                                     "Sobald ein Methodenaufruf abstrakt ist, wird die Basisklasse abstrakt."
+                                     "Fuer die Formatierung der generierten Dateien wird astyle verwendet."
 
-"Sample Usage:"
-"getoptgen [options] ... QUELLE"
-"getoptgen [--out-path] ... QUELLE"
+                                     "Sample Usage:"
+                                     "getoptgen [options] ... QUELLE"
+                                     "getoptgen [--out-path] ... QUELLE"
 
-"Options:"
-" ShortOpt: h LongOpt: help Description: Diese Hilfe ausgeben und beenden"
-" ShortOpt: v LongOpt: - Description: Gibt die Version des Programms aus und beendet"
-" ShortOpt: g LongOpt: generate Description: Gibt XML an die eingelesen werden soll"
- 
-"Kontaktdaten:"
-"Autoren: Jonas Heiss Email: Christoph@familie-gruender.de";const std::string version="tba";std::string path;
+                                     "Options:"
+                                     " ShortOpt: h LongOpt: help Description: Diese Hilfe ausgeben und beenden"
+                                     " ShortOpt: v LongOpt: - Description: Gibt die Version des Programms aus und beendet"
+                                     " ShortOpt: g LongOpt: generate Description: Gibt XML an die eingelesen werden soll"
+                                     " ShortOpt: s LongOpt: signperline Description: Setzte sign per line"
+
+                                     "Kontaktdaten:"
+                                     "Autoren: Jonas Heiss Email: Christoph@familie-gruender.de";
+        const std::string version = "tba";
+        std::string path;
+        int signperline = 79;
 
 
-void parseOptions(int argc, char **argv);
+        void parseOptions(int argc, char **argv);
 
 //Diese Hilfe ausgeben und beenden
-void printhelp();
+        void printhelp();
 
-bool isSethelptext();
+        bool isSethelptext();
 
 //Gibt die Version des Programms aus und beendet
-void printversion();
+        void printversion();
 
-bool isSetversion();
+        bool isSetversion();
 
 //Gibt XML an die eingelesen werden soll
-virtual void generate(std::string path)=0;
+        virtual void generate(std::string path) = 0;
 
-std::string getValueOfpath();
+        std::string getValueOfpath();
 
-bool isSetpath();
+        bool isSetpath();
 
+//Setzte sign per line
+        void setSignPerLine();
+
+        bool isSetsignperline();
+
+    };
 }
-;}
 
 #endif
